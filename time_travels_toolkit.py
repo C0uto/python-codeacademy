96143e93dea07ed82bc9a8ee070a1f91c268d40f
Nuno Couto <49104441+C0uto@users.noreply.github.com>
2025-05-02 19:52:13 +0100

    Update time_travels_toolkit.py

---
+++ time_travels_toolkit.py
@@ -0,0 +1,18 @@
+import datetime as dt
+import custom_module
+from decimal import Decimal
+from random import choice
+from random import randint
+
+current_date_time = dt.datetime.now()
+print(current_date_time)
+
+year = randint(1900,3000)
+
+base_cost = Decimal('1.00')
+cost_multiplier = abs(current_date_time.year - year)
+cost = base_cost * cost_multiplier
+
+travels = choice(['Lisboa', 'Londres', 'Nova Iorque', 'Tokio'])
+
+print(custom_module.generate_time_travel_message(year, travels ,cost))
