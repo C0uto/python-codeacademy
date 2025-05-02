f6658931420865049e55ea9ace70a62f72c47935
Nuno Couto <n.miguel2001@gmail.com>
2025-05-02 18:16:03 +0100

    Delete function_ASCII85decoder.py

--- function_ASCII85decoder.py
+++
@@ -1,9 +0,0 @@
-import base64
-
-encoded_text = "<~FD,B0+DGm>C3=T>+EV:.+D,>.F*&OGFCfDB+CT.u+EV:.+D,b6+DGm>@<6L4Eb/c)~>"
-
-encoded_text = encoded_text[2:-2]
-
-decoded_data = base64.a85decode(encoded_text)
-
-print(decoded_data)
