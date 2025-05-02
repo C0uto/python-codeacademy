d88adb9e10c0582af57abec2d3335e4b246475ee
Nuno Couto <49104441+C0uto@users.noreply.github.com>
2025-05-02 18:28:54 +0100

    Update function_ASCII85Decoder.py

--- function_ASCII85Decoder.py
+++ function_ASCII85Decoder.py
@@ -2,9 +2,9 @@ import base64
 
 def decode_ascii85(encoded_text):
     # Remove the start and end markers
-    encoded_text = encoded_text[2:-2]
+    text_with_nomarkers = encoded_text[2:-2]
 
     # Decode the ASCII85 encoded text
-    decoded_data = base64.a85decode(encoded_text)
+    decoded_data = base64.a85decode(text_with_nomarkers)
 
     return decoded_data
